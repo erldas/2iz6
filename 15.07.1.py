230d5751b75870064d46de2010657cba9e2e6648
erldas <daserlikov@gmail.com>
2020-07-17 05:59:57 +0600

    1

---
+++ 15.07.1.py
@@ -0,0 +1,14 @@
+# 1) Попросить пользователя ввести текст. 
+# В результате вывести процент букв в
+# верхнем регистре (заглавные) и в нижнем регистре (прописные).
+
+a = str(input('Napishite chto nibud : '))
+
+print(list(filter(len()(str.isupper, a))))
+
+
+# data = 'Dastan' , 'Aybek', 'Janara', 'Vasya', 'Arnold', 'Bruce'
+# filtered_data = list(filter(filt, data))
+# print(filtered_data)
+
+
